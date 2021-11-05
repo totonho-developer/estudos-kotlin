11c2c41c6a5041225e11d293e09edc60492a51b8
totonho-developer <desenvolvedor.martins@hotmail.com>
2021-11-04 17:01:36 -0700

    Criando construtor e funcao para pessoa

--- DioInnovationOne/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ DioInnovationOne/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,9 +2,12 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Antonio"
-
     var cpf: String = "123.123.123.12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
@@ -12,4 +15,5 @@ fun main() {
 
     println(tonho.nome)
     println(tonho.cpf)
+    println(tonho.pessoaInfo())
 }
